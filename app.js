6f4df56a300420b5671c28a593ef1ff0f69e1063
Akshay KM <akshay194@gmail.com>
2022-06-27 22:09:26 +0530

    DB added

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose"); 
 const app = express();
-
-let items = ["GYM","BUY FOOD"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemSchema = {
+    name : String
+}
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1 = new Item({
+    name : "WORKOUT"
+})
+const item2 = new Item({
+    name : "BUY FOOD"
+})
+
+const defaultItem = [item1,item2];
+
+
+
 app.get("/",function(req,res){
+
+Item.find({},function(err,foundItems){
+    if (foundItems.length === 0){
+   Item.insertMany(defaultItem,function(err){
+    if (err){
+        console.log(err);
+    }else{
+        console.log("succesfully added to database");
+    }
+     })
+     res.redirect("/");
+    }else{
+        res.render("list",{kindOfDay:day,newListItems:foundItems});
+    }
+    
+})
+
     let today = new Date();
     
     let options = {
@@ -19,13 +53,26 @@ app.get("/",function(req,res){
     }
     let day = today.toLocaleDateString("en-US",options);
 
-    res.render("list",{kindOfDay:day,newListItems:items});
+
 })
 
 app.post("/",function(req,res){
-   let item = req.body.todoitem;
-   items.unshift(item)
-   res.redirect("/") 
+   let itemName = req.body.todoitem;
+   const item = new Item({
+    name : itemName
+   })
+   item.save();
+   res.redirect("/")
+})
+
+app.post("/delete",function(req,res){
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId,function(err){
+      if(!err){
+        console.log("successfully deleted checked item")
+      }
+      res.redirect("/")
+    })
 })
 
 
